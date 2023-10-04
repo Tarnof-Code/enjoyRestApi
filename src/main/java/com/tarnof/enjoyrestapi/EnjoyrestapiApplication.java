109276746780065db094d5defacad4a05b9a7630
Tarnof <tarnof@hotmail.com>
2023-10-04 15:13:02 +0200

    Initialisation du projet

---
+++ src/main/java/com/tarnof/enjoyrestapi/EnjoyrestapiApplication.java
@@ -0,0 +1,13 @@
+package com.tarnof.enjoyrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnjoyrestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnjoyrestapiApplication.class, args);
+	}
+
+}
